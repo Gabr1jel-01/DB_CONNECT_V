00720a24d995e27a67bae91cb9fa1a7b6064bdb1
Gabrijel <76105935+Gabr1jel-01@users.noreply.github.com>
2025-01-17 14:30:40 +0100

    Added pills buttons

--- DatabaseConnect.py
+++ DatabaseConnect.py
@@ -39,6 +39,11 @@ def display_file_in_a_table(data_to_be_displayed):
     st.dataframe(data_to_be_displayed)
 
 with st.sidebar:
+    
+    sidebar_image = st.image("image.png")
+    sidebar_logo = st.logo("LOGO.png",size="large")
+
+
     st.title("File:")
     file_uploader_dragNdrop = st.file_uploader("Choose a file to upload:")
     st.divider()
@@ -51,7 +56,7 @@ with st.sidebar:
 
     #region DOWNLOAD CUSTOM TIME AND DATE FROM THE TABLE
     st.subheader("To download specific table of readings you need to choose the date, time and data:")
-    first_column, second_column = st.columns(2)
+    first_column, second_column, third_column = st.columns(3)
     
     with first_column:
         date_input_date_picker_from = st.date_input("From",
@@ -76,132 +81,9 @@ with st.sidebar:
         time_input_time_picker_to = st.time_input("To:",
                                                 key="time_to",
                                                 value=None)
-    with st.expander("Filters"):
-        column1 = st.columns(1)
-        list_of_data = ["Energy_Total_Reactive_Energy_L1L2L3"
-                        ,"Energy_Total_Active_Power_L1L2L3"
-                        ,"Energy_Total_Apparent_Power_L1L2L3"
-                        ,"Energy_Total_Reactive_Power_L1L2L3"
-                        ,"Energy_Reset_Total_Active_Energy_L1L2L3"
-                        ,"Energy_Reset_Total_Reactive_Energy_L1L2L3"
-                        ,"Energy_Total_Active_Energy_L1L2L3"
-                        ,"Energy_Current_L1"
-                        ,"Energy_Current_L2","Energy_Current_L3"
-                        ,"TT_Reactor_Seals_Cooling_Oil"
-                        ,"TT_Flue_Gas_In"
-                        ,"TT_Flue_Gas_In_2"
-                        ,"TT_Flue_Gas_Out"
-                        ,"TT_Syngas_Afterheater_Out"
-                        ,"TT_Cyclone_1_Outlet"
-                        ,"TT_Syngas_Reactor_Out"
-                        ,"TT_Wet_Scrubber_Middle"
-                        ,"TT_Flue_Gas_Out_2"
-                        ,"TT_Flue_Gas_Out_3"
-                        ,"PT_Hydraulic_Pressure"
-                        ,"PT_Reactor_Inlet"
-                        ,"PT_Wet_Scrubber_Cooling"
-                        ,"PT_Syngas_Reactor_Out"
-                        ,"PT_Cyclone_1_Outlet"
-                        ,"PT_Syngas_Afterheater_Out"
-                        ,"PT_Syngas_Storage_Pressure"
-                        ,"PT_Flue_Gas_In"
-                        ,"PT_Active_Carbon_Outlet"
-                        ,"PT_Active_Carbon_Inlet"
-                        ,"AT_pH_Meter_Filtering_Tanks"
-                        ,"LT_Wet_Scrubber_Outlet"
-                        ,"LT_Wet_Scrubber_Inlet"
-                        ,"FM_Volume_Flow"
-                        ,"FM_Mass_Flow"
-                        ,"FM_Energy_Flow"
-                        ,"FM_Flow_Velocity"
-                        ,"FM_Temperature"
-                        ,"FM_Calorific_Value"
-                        ,"FM_Pressure"
-                        ,"Syngas_CH4"
-                        ,"Syngas_CO"
-                        ,"Syngas_CO2"
-                        ,"Syngas_H2"
-                        ,"Syngas_H2S"
-                        ,"Syngas_N2"
-                        ,"Syngas_O2"
-                        ,"Syngas_Net_Calorific_Value_1"
-                        ,"Syngas_Net_Calorific_Value_2"
-                        ,"Syngas_Gross_Calorific_Value"
-                        ,"Syngas_Temperature"
-                        ,"Syngas_Sample_Flow"
-                        ,"Syngas_Pressure"
-                        ,"Syngas_Mixture_CH4"
-                        ,"Syngas_Mixture_CO"
-                        ,"Syngas_Mixture_CO2"
-                        ,"Syngas_Mixture_H2"
-                        ,"Syngas_Mixture_H2S"
-                        ,"Syngas_Mixture_N2"
-                        ,"Syngas_Mixture_O2"
-                        ,"Machine_State"
-                        ,"VSD_COF_State"
-                        ,"VSD_COF_Freq"
-                        ,"VSD_CONV_State"
-                        ,"VSD_CONV_Freq"
-                        ,"VSD_DP2_State"
-                        ,"VSD_DP2_Freq"
-                        ,"VSD_HYD_State"
-                        ,"VSD_HYD_Freq"
-                        ,"VSD_RSC_State"
-                        ,"VSD_RSC_Freq"
-                        ,"VSD_RSM_State"
-                        ,"VSD_RSM_Freq"
-                        ,"VSD_SBF_State"
-                        ,"VSD_SBF_Freq"
-                        ,"VSD_SBM_State"
-                        ,"VSD_SBM_Freq"
-                        ,"VSD_SBSF_State"
-                        ,"VSD_SBSF_Freq"
-                        ,"VSD_SAMP_State"
-                        ,"VSD_SAMP_Freq"
-                        ,"M_IN_4_3_09_State"
-                        ,"M_IN_4_3_10_State"
-                        ,"M_IN_4_9_01_State"
-                        ,"M_IN_5_1_02_State"
-                        ,"M_IN_5_1_04_State"
-                        ,"M_IN_5_1_31_State"
-                        ,"M_IN_6_1_01_State"
-                        ,"M_IN_6_1_02_State"
-                        ,"M_IN_8_1_01_State"
-                        ,"M_IN_8_2_03_State"
-                        ,"M_IN_8_6_01_State"
-                        ,"M_IN_8_6_02_State"
-                        ,"Afterburner_State"
-                        ,"Afterburner_Power_Feedback"
-                        ,"Feeder_State"
-                        ,"PID_Pressure_Setpoint"
-                        ,"PID_Pressure_Status"
-                        ,"PID_Wet_Scrubber_Setpoint"
-                        ,"PID_Wet_Scrubber_Status"
-                        ,"SV_IN_2_2_21_State"
-                        ,"SV_IN_3_1_21_State"
-                        ,"SV_IN_3_1_22_State"
-                        ,"SV_IN_3_3_21_State"
-                        ,"SV_IN_3_5_21_State"
-                        ,"SV_IN_3_5_22_State"
-                        ,"SV_IN_4_3_21_State"
-                        ,"SV_IN_4_3_60_State"
-                        ,"SV_IN_6_1_21_State"
-                        ,"SV_IN_4_3_11_State"
-                        ,"EMV_IN_5_1_21_State"
-                        ,"EMV_IN_5_1_22_State"
-                        ,"EMV_IN_5_1_23_State"
-                        ,"EMV_IN_5_1_24_State"
-                        ,"EMV_IN_5_1_29_State"
-                        ,"FFV_IN_6_1_03_State"
-                        ,"FFV_IN_6_1_04_State"
-                        ,"FFV_IN_6_1_05_State"
-                        ,"FFV_IN_6_1_06_State"]
-        with column1:
-            st.pills()
-        
     
-    button_download = st.button("DOWNLOAD", type="primary")    
 
+    button_download = st.button("DOWNLOAD", type="primary")
 
     st.divider()
     #endregion
@@ -232,3 +114,156 @@ if file_uploader_dragNdrop is not None:
         
 else:
         st.warning('To start, please load a file', icon="⚠️")
+
+
+
+with st.expander("Filters"):
+        column1, column2, column3, column4, column5, column6, column7, column8, column9, column10  = st.columns(10)
+        list_of_data_first_column = ["Total_Reactive_Energy"
+                        ,"Energy_Total_Active_Power_L1L2L3"
+                        ,"Energy_Total_Apparent_Power_L1L2L3"
+                        ,"Energy_Total_Reactive_Power_L1L2L3"
+                        ,"Energy_Reset_Total_Active_Energy_L1L2L3"
+                        ,"Energy_Reset_Total_Reactive_Energy_L1L2L3"
+                        ,"Energy_Total_Active_Energy_L1L2L3"
+                        ,"Energy_Current_L1"
+                        ,"Energy_Current_L2","Energy_Current_L3"
+                        ,"TT_Reactor_Seals_Cooling_Oil"
+                        ,"TT_Flue_Gas_In"
+                        ,"TT_Flue_Gas_In_2"
+                        ]
+        list_of_data_second_column = ["M_IN_5_1_31_State"
+                        ,"M_IN_6_1_01_State"
+                        ,"M_IN_6_1_02_State"
+                        ,"M_IN_8_1_01_State"
+                        ,"M_IN_8_2_03_State"
+                        ,"M_IN_8_6_01_State"
+                        ,"M_IN_8_6_02_State"
+                        ,"Afterburner_State"
+                        ,"Afterburner_Power_Feedback"
+                        ,"Feeder_State"
+                        ,"PID_Pressure_Setpoint"
+                        ,"PID_Pressure_Status"]
+        list_of_data_third_column = ["TT_Flue_Gas_Out"
+                        ,"TT_Syngas_Afterheater_Out"
+                        ,"TT_Cyclone_1_Outlet"
+                        ,"TT_Syngas_Reactor_Out"
+                        ,"TT_Wet_Scrubber_Middle"
+                        ,"TT_Flue_Gas_Out_2"
+                        ,"TT_Flue_Gas_Out_3"
+                        ,"PT_Hydraulic_Pressure"
+                        ,"PT_Reactor_Inlet"
+                        ,"PT_Wet_Scrubber_Cooling"
+                        ,"PT_Syngas_Reactor_Out"
+                        ,"PT_Cyclone_1_Outlet"]
+        list_of_data_fourth_column = ["VSD_HYD_Freq"
+                        ,"VSD_RSC_State"
+                        ,"VSD_RSC_Freq"
+                        ,"VSD_RSM_State"
+                        ,"VSD_RSM_Freq"
+                        ,"VSD_SBF_State"
+                        ,"VSD_SBF_Freq"
+                        ,"VSD_SBM_State"
+                        ,"VSD_SBM_Freq"
+                        ,"VSD_SBSF_State"
+                        ,"VSD_SBSF_Freq"
+                        ,"VSD_SAMP_State"
+                        ]
+        list_of_data_fift_column = ["VSD_SAMP_Freq"
+                        ,"M_IN_4_3_09_State"
+                        ,"M_IN_4_3_10_State"
+                        ,"M_IN_4_9_01_State"
+                        ,"M_IN_5_1_02_State"
+                        ,"M_IN_5_1_04_State"
+                        ,"PID_Wet_Scrubber_Setpoint"
+                        ,"PID_Wet_Scrubber_Status"
+                        ,"SV_IN_2_2_21_State"
+                        ,"SV_IN_3_1_21_State"
+                        ,"SV_IN_3_1_22_State"
+                        ,"SV_IN_3_3_21_State"]
+        list_of_data_sixth_column = ["SV_IN_4_3_60_State"
+                        ,"SV_IN_6_1_21_State"
+                        ,"SV_IN_4_3_11_State"
+                        ,"EMV_IN_5_1_21_State"
+                        ,"EMV_IN_5_1_22_State"
+                        ,"EMV_IN_5_1_23_State"
+                        ,"EMV_IN_5_1_24_State"
+                        ,"EMV_IN_5_1_29_State"
+                        ,"FFV_IN_6_1_03_State"
+                        ,"FFV_IN_6_1_04_State"
+                        ,"FFV_IN_6_1_05_State"
+                        ,"FFV_IN_6_1_06_State"]
+        list_of_data_seventh_column = ["SV_IN_3_5_21_State"
+                        ,"SV_IN_3_5_22_State"
+                        ,"SV_IN_4_3_21_State"
+                        ,"Syngas_Net_Calorific_Value_1"
+                        ,"Syngas_Net_Calorific_Value_2"
+                        ,"Syngas_Gross_Calorific_Value"
+                        ,"Syngas_Temperature"
+                        ,"Syngas_Sample_Flow"
+                        ,"Syngas_Pressure"
+                        ,"Syngas_Mixture_CH4"
+                        ,"Syngas_Mixture_CO"
+                        ,"Syngas_Mixture_CO2"]
+        list_of_data_eighth_column = ["Syngas_N2"
+                        ,"Syngas_O2"
+                        ,"Syngas_Mixture_H2S"
+                        ,"Syngas_Mixture_N2"
+                        ,"Syngas_Mixture_O2"
+                        ,"Machine_State"
+                        ,"VSD_COF_State"
+                        ,"VSD_COF_Freq"
+                        ,"VSD_CONV_State"
+                        ,"VSD_CONV_Freq"
+                        ,"VSD_DP2_State"
+                        ,"VSD_DP2_Freq"
+                        ]
+        list_of_data_nineth_column = ["LT_Wet_Scrubber_Inlet"
+                        ,"FM_Volume_Flow"
+                        ,"FM_Mass_Flow"
+                        ,"VSD_HYD_State"
+                        ,"Syngas_Mixture_H2"
+                        ,"FM_Energy_Flow"
+                        ,"FM_Flow_Velocity"
+                        ,"FM_Temperature"
+                        ,"FM_Calorific_Value"
+                        ,"FM_Pressure"
+                        ,"Syngas_CH4"
+                        ,"Syngas_CO"]
+        list_of_data_tenth_column = ["PT_Syngas_Afterheater_Out"
+                        ,"PT_Syngas_Storage_Pressure"
+                        ,"PT_Flue_Gas_In"
+                        ,"PT_Active_Carbon_Outlet"
+                        ,"PT_Active_Carbon_Inlet"
+                        ,"AT_pH_Meter_Filtering_Tanks"
+                        ,"LT_Wet_Scrubber_Outlet"
+                        ,"Syngas_CO2"
+                        ,"Syngas_H2"
+                        ,"Syngas_H2S"]
+      
+
+        with column1:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_first_column],label_visibility="hidden")
+        with column2:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_second_column],label_visibility="hidden")
+        with column3:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_third_column],label_visibility="hidden")
+        with column4:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_fourth_column],label_visibility="hidden")
+        with column5:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_fift_column],label_visibility="hidden")
+        with column6:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_sixth_column],label_visibility="hidden")
+        with column7:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_seventh_column],label_visibility="hidden")
+        with column8:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_eighth_column],label_visibility="hidden")
+        with column9:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_nineth_column],label_visibility="hidden")
+        with column10:
+            st.pills("a",selection_mode="multi",options=[item for item in list_of_data_tenth_column],label_visibility="hidden")
+        st.divider()
+        
+        button_generate = st.button("GENERATE", type="primary")
+
+st.table()
